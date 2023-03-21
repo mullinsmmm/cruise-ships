4cc2c83fa6b1dba1e82afbe576ee640ec8428435
Matt Mullins <matt.mullins@live.com>
2023-03-21 21:00:42 +0000

    Port Object Instanced

--- __tests__/cruiseShips.test.js
+++ __tests__/cruiseShips.test.js
@@ -1,5 +1,6 @@
 /*globals describe it expect */
 const Ship = require('../src/cruiseShips.js');
+const Port = require('../src/cruiseShips.js');
 
 describe ('Ship', () => {
     xit('can be initiated', () => {
@@ -12,13 +13,17 @@ describe ('Ship', () => {
         expect(boat1.startingPort).toBe('Portsmouth')
     });
 
-    it('can set sail', () => {
+    xit('can set sail', () => {
         const boat1 = new Ship('Portsmouth');
     
         boat1.setSail();
     
         expect(boat1.startingPort).toBeFalsy();
      });
+});
 
-
+describe ('Port', () => {
+    it('can be instantiated', () => {
+        expect(new Port()).toBeInstanceOf(Object);
+    })
 });
--- src/cruiseShips.js
+++ src/cruiseShips.js
@@ -3,16 +3,22 @@ const startingPort = ['Portsmouth'];
 class Ship {
     constructor(name) {
       this.name = name,
-      this.startingPort = startingPort,
-      this.dock = dock
-    }
+      this.startingPort = startingPort
+    };
 
     setSail(){
         this.startingPort = '';
-    }
-}
+    };
+};
+
+class Port {
+    constructor(name) {
+        this.name = name
+    };
+};
 
 const boat1 = new Ship('Ferry');
+const port1 = new Port('Calais');
 
 
 module.exports = Ship;
