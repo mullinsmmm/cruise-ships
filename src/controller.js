7f0df51d3efc0aacad90432270245250cb4a944c
Matt Mullins <matt.mullins@live.com>
2023-03-25 14:08:35 +0000

    Add Ship

--- src/controller.js
+++ src/controller.js
@@ -35,6 +35,14 @@ Controller.prototype = {
         portsElement.style.width = `${portsElementWidth + 256}px`;
         });
     },
+
+    renderShip(ship) {
+        const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+        const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+        const shipElement = document.querySelector('#ship');
+        shipElement.style.top = '${portElement.offsetTop + 32}px';
+        shipElement.style.left = '${portElement.offsetLeft - 32}px';
+    }
 };
 
         if(typeof module !== 'undefined' && module.exports) {
